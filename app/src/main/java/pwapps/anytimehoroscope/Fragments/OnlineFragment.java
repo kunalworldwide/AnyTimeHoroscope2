233cb8505fba2bfe917a305a01ee605e7548d345
kunalworldwide <kunalworldwide@gmail.com>
2019-07-11 13:24:19 +0530

    Online section working

--- app/src/main/java/pwapps/anytimehoroscope/Fragments/OnlineFragment.java
+++ app/src/main/java/pwapps/anytimehoroscope/Fragments/OnlineFragment.java
@@ -1,5 +1,7 @@
 package pwapps.anytimehoroscope.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,30 +9,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pwapps.anytimehoroscope.OnlineResults;
 import pwapps.anytimehoroscope.R;
 import pwapps.anytimehoroscope.RegistrationForm;
 
 import static android.content.Context.MODE_PRIVATE;
 
 
-public class OnlineFragment extends Fragment {
+public class OnlineFragment extends Fragment  implements View.OnClickListener {
+    Button btn;
+    TextView txt;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View fragmentView =inflater.inflate(R.layout.fragment_online,container,false);
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        View fragmentView = inflater.inflate(R.layout.fragment_online, container, false);
 
-
-
+        btn = (Button) fragmentView.findViewById(R.id.onlinebtn);
+        // txt=(TextView)fragmentView.findViewById(R.id.resultText);
+        btn.setOnClickListener(this);
+        //txt.setText(getresult());
 
         return fragmentView;
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.onlinebtn:
+                Intent i = new Intent(getActivity(), OnlineResults.class);
+                startActivity(i);
+                break;
+        }
+    }
+
+
 }
+
+   /* private String getresult() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("horoscope", MODE_PRIVATE);
+        String result = sharedPreferences.getString("Result", "");
+        return result;
+    }
+}*/
